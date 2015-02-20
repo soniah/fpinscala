311b15edc35f90546c0100545869dab5a81218de
Sonia Hamilton <sonia@snowfrog.net>
2015-02-20 13:53:03 +1100

    toList, whitespace

--- exercises/src/main/scala/fpinscala/laziness/LazinessMain.scala
+++ exercises/src/main/scala/fpinscala/laziness/LazinessMain.scala
@@ -3,6 +3,9 @@ package fpinscala.laziness
 object LazinessMain {
   import fpinscala.laziness._
   def main(args: Array[String]): Unit = {
-    println("Hello Lazy World!")
+
+    val l = Stream(1,2,3)
+    println("toList: " + (l.toList))
+
   }
 }
--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -9,7 +9,7 @@ trait Stream[+A] {
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -31,6 +31,10 @@ trait Stream[+A] {
   // writing your own function signatures.
 
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
+
+  // solutions: use tailrec + List.reverse
+  def toList: List[A] = foldRight(List[A]())(_ :: _)
+
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -45,7 +49,7 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
