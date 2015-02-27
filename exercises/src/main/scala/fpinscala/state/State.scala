414f64d615cb31ea44dd24d1722d74fadbf63813
Sonia Hamilton <sonia@snowfrog.net>
2015-02-27 18:02:13 +1100

    6.0 tidy

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -2,18 +2,33 @@ package fpinscala.state
 
 
 trait RNG {
-  def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
+  // Should generate a random `Int`. We'll later define other
+  // functions in terms of `nextInt`.
+  def nextInt: (Int, RNG)
 }
 
 object RNG {
   // NB - this was called SimpleRNG in the book text
-
   case class Simple(seed: Long) extends RNG {
+
     def nextInt: (Int, RNG) = {
-      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
-      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
-      val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
-      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+
+      // `&` is bitwise AND. We use the current seed to generate
+      // a new seed.
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) &
+        0xFFFFFFFFFFFFL
+
+      // The next state, which is an `RNG` instance created from
+      // the new seed.
+      val nextRNG = Simple(newSeed)
+
+      // `>>>` is right binary shift with zero fill. The value
+      // `n` is our new pseudo-random integer.
+      val n = (newSeed >>> 16).toInt
+
+      // The return value is a tuple containing both a
+      // pseudo-random integer and the next `RNG` state.
+      (n, nextRNG)
     }
   }
 
