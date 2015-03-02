918ada21f3d341e2667fa3a7f0fb9df545e87133
Sonia Hamilton <sonia@snowfrog.net>
2015-03-03 09:33:27 +1100

    6.10 sequence sig

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -160,8 +160,6 @@ case class State[S,+A](run: S => (A, S)) {
       f(a).run(s2)
     })
 
-  // def sequence -> in object State
-
 }
 
 // ===============================================
@@ -175,6 +173,9 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 object State {
 
   def unit[S,A](a: A): State[S,A] = State(s => (a,s))
+
+  def sequence[S,A](ss: List[State[S,A]]): State[S,List[A]] =
+    sys.error("todo")
 
   type Rand[A] = State[RNG, A]
 
