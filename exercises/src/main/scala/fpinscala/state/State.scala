e076b989a001f2eba4f09cef3a80f5e42be0938e
Sonia Hamilton <sonia@snowfrog.net>
2015-03-02 19:49:41 +1100

    6.10 map

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -149,7 +149,7 @@ object RNG {
 case class State[S,+A](run: S => (A, S)) {
 
   def map[B](f: A => B): State[S, B] =
-    sys.error("todo")
+    this.flatMap{ i => State.unit(f(i)) }
 
   def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     sys.error("todo")
